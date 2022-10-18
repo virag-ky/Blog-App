9a8a04e78394adbaaa6876f5dfb55d4f246f3cb5
Virag Kormoczy <vkormoczy@gmail.com>
2022-10-18 13:39:10 +0300

    Add users_id to Posts table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_18_103513) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_18_103810) do
   create_table "comments", force: :cascade do |t|
     t.string "text"
     t.datetime "created_at", null: false
@@ -33,6 +33,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_18_103513) do
     t.integer "likes_counter"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -46,4 +48,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_18_103513) do
 
   add_foreign_key "comments", "posts"
   add_foreign_key "comments", "users"
+  add_foreign_key "posts", "users"
 end
